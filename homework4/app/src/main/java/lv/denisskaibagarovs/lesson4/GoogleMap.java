e48eb4cf022ccf935d89048e92739c5f89f45b68
bboyandry <dryshabot>
2019-04-03 23:38:09 +0300

    Homework 4 Work with libs.. okHttp, Gson, Piccaso, Butterknife

---
+++ homework4/app/src/main/java/lv/denisskaibagarovs/lesson4/GoogleMap.java
@@ -0,0 +1,21 @@
+package lv.denisskaibagarovs.lesson4;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class GoogleMap extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_googlemap);
+
+        Intent intent = getIntent();
+        String value = intent.getStringExtra("key");
+
+        TextView textView = findViewById(R.id.textViewGoogleMap);
+        textView.setText(value);
+    }
+}å
---
+++ homework4/app/src/main/java/lv/denisskaibagarovs/lesson4/ViewHolder.java
@@ -0,0 +1,17 @@
+package lv.denisskaibagarovs.lesson4;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ViewHolder {
+    @BindView(R.id.imageView) ImageView imageView;
+    @BindView(R.id.textViewAuthor) TextView textViewAuthor;
+    @BindView(R.id.textViewLocation) TextView textViewLocation;
+
+
+    public ViewHolder (View view) { ButterKnife.bind(this, view); }
+}
